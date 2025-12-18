3c4ce95dfcfe22a3ecb5c8a1cdb7dfbdfc1b3ab0
Omid Farhang <boelectronic@gmail.com>
2025-12-19 01:41:19 +0330

    cleanup

--- themes/omid-dev/assets/css/core/license.css
+++ themes/omid-dev/assets/css/core/license.css
@@ -1,6 +1,5 @@
 /*
-  PaperMod v7
-  License: MIT https://github.com/adityatelange/hugo-PaperMod/blob/master/LICENSE
-  Copyright (c) 2020 nanxiaobei and adityatelange
-  Copyright (c) 2021-2024 adityatelange
+  omid-dev v1
+  License: MIT https://omid.dev/
+  Copyright (c) 2025 Omid Farhang
 */
